1abd52de9016cf636b8a0862654863ec2fb73dc6
Svidon <nicogira95@gmail.com>
2018-04-10 17:00:06 +0200

    Symbol tables, fst in developement

--- language_model.py
+++ language_model.py
@@ -1,9 +1,12 @@
-# Read file's data
+# Open needed files
 f = open('data/NLSPARQL.train.data', 'r')
+w_out = open('I.lex', 'w')
+t_out = open('O.lex', 'w')
+automa = open('a.txt', 'w')
 
 # Sentences of corpus
 sents = []
-tmp = []
+tmp = [tuple(['<s>', 'O'])]
 
 # Identify all sentences with tuples of word-tag
 for line in f:
@@ -13,11 +16,12 @@ for line in f:
 		tmp.append(tuple(a))
 	else:
 		sents.append(tmp)
-		tmp = []
+		tmp = [tuple(['<s>', 'O'])]
 
 
-# Dictionary of words frequencies
+# Dictionary of words frequencies and tag frequencies
 word_freq = {}
+tag_freq = {}
 
 # Dictionary of word-tag counts
 word_tag_count = {}
@@ -31,6 +35,8 @@ for p in sents:
 		key = t[0] + ' ' + t[1]
 		word_tag_count[key] = word_tag_count.get(key, 0) + 1
 		word_freq[t[0]] = word_freq.get(t[0], 0) + 1
+		tag_freq[t[1]] = tag_freq.get(t[1], 0) + 1
+		
 
 # Fill probabilities dictionary
 for p in sents:
@@ -39,4 +45,29 @@ for p in sents:
 		if key not in word_tag_prob:
 			word_tag_prob[key] = word_tag_count[key]/word_freq[t[0]]
 
-print(word_tag_prob)
+
+# Create vocabulary files
+ids = 1
+
+# Add epsilon
+ttmp = '<eps>' + ' ' + '0' + '\n'
+t_out.write(ttmp)
+
+wtmp = '<eps>' + ' ' + '0' + '\n'
+w_out.write(wtmp)
+
+
+for w_key, t_key in zip(word_freq, tag_freq):
+	ttmp = t_key + ' ' + str(ids) + '\n'
+	t_out.write(ttmp)
+
+	wtmp = w_key + ' ' + str(ids) + '\n'
+	w_out.write(wtmp)
+
+	ids += 1
+
+unk = '<unk>' + ' ' + str(ids) + '\n'
+w_out.write(unk)
+
+
+# Create automaton
